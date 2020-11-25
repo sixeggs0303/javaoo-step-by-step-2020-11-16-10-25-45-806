932405fc82823ae560a0427c5f8e3edb3ebaba19
Marcus Luk <marcus.luk@oocl.com>
2020-11-25 14:54:11 +0800

    [OOP-Practice][Marcus Luk] feat: finished task 5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,24 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = null;
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass(){
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. " + (getKlass() == null ? "I teach No Class." : "I teach Class " + getKlass() + ".");
+    }
 }
