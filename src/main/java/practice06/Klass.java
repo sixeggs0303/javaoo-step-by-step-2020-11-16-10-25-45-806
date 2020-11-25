f0183826f0f781e670ecfe2a67c001de4a71a5a7
Marcus Luk <marcus.luk@oocl.com>
2020-11-25 15:05:30 +0800

    [OOP-Practice][Marcus Luk] feat: finished task 6

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,21 @@
 package practice06;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
