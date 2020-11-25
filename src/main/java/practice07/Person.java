77e0a535389802d3d7597dc4733b124170ea3d91
Marcus Luk <marcus.luk@oocl.com>
2020-11-25 15:24:10 +0800

    [OOP-Practice][Marcus Luk] feat: finished task 7

--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,34 @@
 package practice07;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is " + getName() + ". I am " + getAge() + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Person person = (Person) obj;
+        return person.getId() == this.getId();
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,25 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        String result = super.introduce() + " I am a Student.";
+        if(this.getKlass().getLeader() != null && this.getKlass().getLeader().equals(this)) {
+            return result +  " I am Leader of " + this.getKlass().getDisplayName() + ".";
+        }
+        else{
+            return result +  " I am at " + this.getKlass().getDisplayName() + ".";
+        }
+    }
 }
